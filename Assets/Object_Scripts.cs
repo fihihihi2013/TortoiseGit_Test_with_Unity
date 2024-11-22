5daa86346d829bdb29b173894894bbcb4f87ed93
heeo-h <iglosslove0@gmail.com>
2024-11-22 12:02:08 +0900

    큐브 생성했습니다~

---
+++ Assets/Object_Scripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("저는 큐브입니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
